6e2613c376f762ce3bee5db03892f1f6326c2d3f
Coubiac <benoit.grisot@gmail.com>
2020-07-23 20:31:45 +0200

    ajout methode pour optimiser batch

---
+++ src/main/java/com/library/emprunts/configuration/RepositoryConfig.java
@@ -0,0 +1,15 @@
+package com.library.emprunts.configuration;
+
+import com.library.emprunts.data.EmpruntEntity;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+
+
+@Configuration
+public class RepositoryConfig implements RepositoryRestConfigurer {
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+        config.exposeIdsFor(EmpruntEntity.class);
+    }
+}
--- src/main/java/com/library/emprunts/repository/EmpruntEntityRepository.java
+++ src/main/java/com/library/emprunts/repository/EmpruntEntityRepository.java
@@ -4,10 +4,17 @@ import com.library.emprunts.data.EmpruntEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
+import java.util.List;
 
 @RepositoryRestResource(path = "emprunts")
 public interface EmpruntEntityRepository extends PagingAndSortingRepository<EmpruntEntity, Long> {
     Page<EmpruntEntity> findByExemplaireBarcode(String exemplaireBarcode, Pageable paging);
     Page<EmpruntEntity> findEmpruntEntitiesByUserId(String userId, Pageable paging);
+    List<EmpruntEntity> findEmpruntEntitiesByDateEmpruntAfter(@DateTimeFormat(pattern = "yyyy-MM-dd")@Param("dateEmprunt")Date dateEmprunt);
+    List<EmpruntEntity> findEmpruntEntitiesByDateEmpruntBeforeAndDateRetourIsNull(@DateTimeFormat(pattern = "yyyy-MM-dd")@Param("dateEmprunt")Date dateEmprunt);
 }
